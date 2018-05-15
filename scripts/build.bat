67dcef99fbfc5529224771dcd8cc8c8d3f5ab16e
Itokatsu <itokatsu@tutanota.com>
2018-05-15 22:18:27 +0200

    fix dlls

--- scripts/build.bat
+++ scripts/build.bat
@@ -15,5 +15,8 @@ if not exist SDL2.dll xcopy /y %libDIR%\SDL2-2.0.8\bin\SDL2.dll .
 if not exist SDL2_image.dll xcopy /y %libDIR%\SDL2_image-2.0.3\bin\SDL2_image.dll .
 if not exist SDL2_ttf.dll xcopy /y %libDIR%\SDL2_ttf-2.0.14\bin\SDL2_ttf.dll .
 if not exist SDL2_mixer.dll xcopy /y %libDIR%\SDL2_mixer-2.0.2\bin\SDL2_mixer.dll .
+
 if not exist libpng16-16.dll xcopy /y %libDIR%\SDL2_image-2.0.3\bin\libpng16-16.dll .
 if not exist zlib1.dll xcopy /y %libDIR%\SDL2_image-2.0.3\bin\zlib1.dll .
+if not exist libfreetype-6.dll xcopy /y %libDIR%\SDL2_ttf-2.0.14\bin\libfreetype-6.dll .
+if not exist libmpg123-0.dll xcopy /y %libDIR%\SDL2_mixer-2.0.2\bin\libmpg123-0.dll .
